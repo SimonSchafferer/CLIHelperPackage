3dda1fe6cde70194f453b1077a03d49647c31f69
SimonSchafferer <schafferer.simon@gmail.com>
2015-01-29 18:47:47 +0100

    RFAM added

--- demo/CodeForSplittingAndMerging.R
+++ demo/CodeForSplittingAndMerging.R
@@ -5,7 +5,7 @@ ORDER = c("CLIApplication.R", "OutResultReference.R", "FilesOutput.R",
   "FoldersOutput.R", "CmdGenResult.R", "CmdGenResultExec.R", "Cutadapt_CLI.R", 
   "HTSeqCount_CLI.R", "IntersectBed_CLI.R","MergeBedFile_CLI.R", 
   "MultiBamCov_CLI.R", "Samtools_CLI.R","BamToBed_CLI.R","SortFiles_CLI.R", 
-  "Bowtie2_CLI.R", "Bowtie2TophatIon_CLI.R", "Tophat2_CLI.R","Bowtie_CLI.R", "MultiIntersectBed_CLI.R")
+  "Bowtie2_CLI.R", "Bowtie2TophatIon_CLI.R", "Tophat2_CLI.R","Bowtie_CLI.R", "MultiIntersectBed_CLI.R", "MultiIntersectBed_perl_CLI.R")
 
 test[ORDER]
 
---
+++ demo/splitted/MultiIntersectBed_perl_CLI.R
@@ -0,0 +1,96 @@
+#--split MultiIntersectBed_perl_CLI
+#'@include IntersectBed_CLI
+#'@title MultiIntersectBed_perl_CLI
+#'@section Slots: 
+#'  \describe{
+#'    \item{\code{slot1}:}{inFilePath \code{"character"}}
+#'    \item{\code{slot2}:}{inFileNames \code{"character"}}
+#'    \item{\code{slot3}:}{cliParams \code{"character"}}
+#'    \item{\code{slot4}:}{outFilePath \code{"character"}}
+#'    \item{\code{slot5}:}{outputFlag \code{"character"}}
+#'    \item{\code{slot6}:}{groupVect \code{"numeric"}}
+#'    \item{\code{slot7}:}{outFileName \code{"character"}}
+#'  }
+#' @name MultiIntersectBed_perl_CLI-class
+#' @export
+setClass("MultiIntersectBed_perl_CLI", contains = "CLIApplication", representation(groupVect="numeric", outFileName="character"))
+
+
+#' @title Constructor method for MultiIntersectBed_perl_CLI
+#' @param inFilePath (inFileNames may also be specified otherwise they will be fetched by list.files)
+#' @export
+#' @docType methods
+setGeneric("MultiIntersectBed_perl_CLI", function(inFilePath, inFileNames, cliParams, outputFlag, outFilePath, groupVect,outFileName){standardGeneric("MultiIntersectBed_perl_CLI")})
+
+setMethod("MultiIntersectBed_perl_CLI", signature(inFilePath="character", inFileNames="character", cliParams="missing", 
+                                             outputFlag="character", outFilePath="character",groupVect="numeric",outFileName="character"),
+          function(inFilePath, inFileNames, cliParams, outputFlag, outFilePath, groupVect, outFileName ){
+            cliParams=""
+            return(new("MultiIntersectBed_perl_CLI", inFilePath=inFilePath, inFileNames=inFileNames, cliParams="", 
+                       outputFlag=outputFlag, outFilePath=outFilePath, groupVect=groupVect, outFileName=outFileName ))
+          })
+setMethod("MultiIntersectBed_perl_CLI", signature(inFilePath="character", inFileNames="character", cliParams="missing", 
+                                                  outputFlag="character", outFilePath="character",groupVect="missing",outFileName="character"),
+          function(inFilePath, inFileNames, cliParams, outputFlag, outFilePath, groupVect, outFileName ){
+            groupVect = numeric()
+            cliParams=""
+            return(new("MultiIntersectBed_perl_CLI", inFilePath=inFilePath, inFileNames=inFileNames, cliParams=cliParams, 
+                       outputFlag=outputFlag, outFilePath=outFilePath, groupVect=groupVect, outFileName=outFileName ))
+          })
+
+#' @title Accessor getGroupVect
+#' @export
+#' @docType methods
+#' @return groupVect
+setGeneric("getGroupVect", function(object){standardGeneric("getGroupVect")})
+setMethod("getGroupVect",signature(object="MultiIntersectBed_perl_CLI"),function(object) {
+  slot(object, "groupVect")
+})
+
+#' @title Accessor getOutFileName
+#' @export
+#' @docType methods
+#' @return outFileName
+setMethod("getOutFileName",signature(object="MultiIntersectBed_perl_CLI"),function(object) {
+  slot(object, "outFileName")
+})
+
+
+#' @title Generates the commands and file for MultiIntersectBed
+#' 
+#' @param MultiIntersectBed_perl_CLI
+#' @return CmdGenResult
+#' @rdname generateCommandResult-method
+#' @export
+setMethod("generateCommandResult",signature(object="MultiIntersectBed_perl_CLI"),function(object) { 
+  #bedtools multiinter [OPTIONS] -i FILE1 FILE2 .. FILEn
+  if( length(getInFilePath(object)) > 1 & length(getInFilePath(object)) != length(getInFileNames(object))  ){
+    warning("Either one file path is given, containnig all files, or the file path vector must match the filename vector in length!")
+  }
+  if( length(getGroupVect(object)) > 1 & length(getGroupVect(object)) != length(getInFileNames(object))  ){
+    warning("Either one file path is given, containnig all files, or the group vector must match the filename vector in length!")
+  }
+  
+  inFN = getInFileNames(object)
+  
+  inFNtrimmed = sub("\\..+$","",inFN)
+  inFilending = sub( inFNtrimmed[1], "", inFN[1]  )
+  outFN = paste0( getOutFileName(object), inFilending)
+  
+  cmd1 = paste0("cd ", getOutFilePath(object))
+  
+  if(length(getGroupVect(object)) == 0){
+    cmd2 = paste0( "perl ",system.file("data/multiIntersectBed_helper.pl",package="CLIHelperPackage"),
+                   " -o ", file.path(getOutFilePath(object),outFN ),
+                   " -i ", paste0( file.path(getInFilePath(object), getInFileNames(object) ),collapse=" ") )    
+  } else{
+    cmd2 = paste0( "perl ",system.file("data/multiIntersectBed_helper.pl",package="CLIHelperPackage"),
+                   " -o ", file.path(getOutFilePath(object),outFN ),
+                   " -g ", paste0(getGroupVect(object),collapse=" "),
+                   " -i ", paste0( file.path(getInFilePath(object), getInFileNames(object) ),collapse=" ") )
+  }
+  
+  res = CmdGenResult(CLIApplication = object, OutResultReference = FilesOutput(outFN), commands = c(cmd1, cmd2))
+  
+  return(res)
+})
